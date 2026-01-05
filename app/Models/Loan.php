ae37e2c5493389d490c59bc52c112f3b3cfb1f67
DestinyGodwin <destinyogodwin.dg@gmail.com>
2026-01-05 18:38:13 +0100

    added loan model and migration

---
+++ app/Models/Loan.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Loan extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'principal',
+        'interest_rate',
+        'duration_years',
+        'total_payable',
+        'status',
+        'approved_by',
+        'approved_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+}
